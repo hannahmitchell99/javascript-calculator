c3f465894e40fb5ac8c54afaa7861e43bc84beb1
Hannah <hannahamitchell99@gmail.com>
2023-01-19 18:44:08 +0200

    added new js functions

--- main.js
+++ main.js
@@ -11,19 +11,29 @@ const multiply = document.querySelector("#multiply");
 const divide = document.querySelector("#divide");
 
 const clear = document.querySelector("#clear");
-const deleteClicked =  document.querySelector("#delete")
+const deleteItem =  document.querySelector("#delete")
 const equal = document.querySelector("#equal")
 const float = document.querySelector("#float")
 
 
-// Functions
+
+//Declaring Functions
 
 const whenClicked = (event) => {
-  display.innerText += event.target.innerText;
+    display.innerText += event.target.innerText;
+    
 };
 
+const clearClicked = (event) => {
+    display.innerText = 0;
+}
 
-//
+const deleteClicked = (event) =>{
+    display.innerText = display.innerText.slice(0,-1)
+}
+
+
+//Calling Functions
 
 numberButtons.forEach((numberButton) => {
   numberButton.addEventListener("click", whenClicked);
@@ -31,3 +41,9 @@ numberButtons.forEach((numberButton) => {
 operatorButtons.forEach((operatorButton) => {
   operatorButton.addEventListener("click", whenClicked);
 });
+
+float.addEventListener("click", whenClicked)
+
+clear.addEventListener("click", clearClicked)
+
+deleteItem.addEventListener("click",deleteClicked)
